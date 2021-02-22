ab3b2895dae7883463166547fdfb106582d973ae
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-22 18:12:39 +0000

    added semicolons to css and added brackets to javascript

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ function comparison () {
   day = document.getElementById('input').value
   age = document.getElementById('input1').value
   age = parseInt(age)
-  if (( day == 'Tuesday' || day == 'Thursday') || age > 12 && age < 21) {
-  document.getElementById('answer').innerHTML = 'You are eligible for student pricing.'   
+  if ((day === 'Tuesday') || (day === 'Thursday') || age > 12 && age < 21) {
+    document.getElementById('answer').innerHTML = 'You are eligible for student pricing.'
   } else {
     document.getElementById('answer').innerHTML = 'You must pay the regular price.'
-    }
   }
-  
+}
